a17d6769b0b747bdbf6748286f15a78d7c1019a2
Amitav Pusty <126335408+pusty1830@users.noreply.github.com>
2025-07-25 15:45:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,9 +41,9 @@ sequelize
 app.use("/api", require("./routes"));
 app.use(express.static(buildPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(buildPath, "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(buildPath, "index.html"));
+// });
 
 app.listen(port, () => {
   console.log("Your Server running at " + port);
